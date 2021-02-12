375f9b0f329fbe40ba95f40ef988b86144562a49
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-02-12 22:19:21 +0100

    spin in both directions

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const btnRight = document.querySelector(".btn-left");
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
 
 function move(item, from, to) {
   console.log("animation done");
@@ -53,5 +54,46 @@ function rotateEggsToRight() {
     console.log("no egg in slot3");
   }
 }
+function rotateEggsToLeft() {
+  const eggInSlot1 = document.querySelector(".slot-one");
+  const eggInSlot2 = document.querySelector(".slot-two");
+  const eggInSlot3 = document.querySelector(".slot-three");
 
+  if (eggInSlot1) {
+    eggInSlot1.style.animation = `1s ease one2three forwards`;
+    eggInSlot1.addEventListener(
+      "animationend",
+      () => {
+        return move(eggInSlot1, "one", "three");
+      },
+      { once: true }
+    );
+  }
+
+  if (eggInSlot2) {
+    eggInSlot2.style.animation = `1s ease two2one forwards`;
+    eggInSlot2.addEventListener(
+      "animationend",
+      () => {
+        return move(eggInSlot2, "two", "one");
+      },
+      {
+        once: true,
+      }
+    );
+  }
+
+  if (eggInSlot3) {
+    eggInSlot3.style.animation = `1s ease three2two forwards`;
+    eggInSlot3.addEventListener(
+      "animationend",
+      () => {
+        return move(eggInSlot3, "three", "two");
+      },
+      { once: true }
+    );
+  }
+}
+
+btnLeft.addEventListener("click", rotateEggsToLeft);
 btnRight.addEventListener("click", rotateEggsToRight);
