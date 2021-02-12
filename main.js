03c981d5e444bbe0db5941c1353fa1dcadb4ec4c
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-02-12 21:42:24 +0100

    made simple spinning

--- main.js
+++ main.js
@@ -1,46 +1,57 @@
-const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-left");
 
-function rotateEggs() {
-  const eggInSlot1 = document.querySelector(".slot1");
-  const eggInSlot2 = document.querySelector(".slot2");
-  const eggInSlot3 = document.querySelector(".slot3");
+function move(item, from, to) {
+  console.log("animation done");
+  item.classList.remove(`slot-${from}`);
+  item.classList.add(`slot-${to}`);
+  item.style.animation = "";
+}
+
+function rotateEggsToRight() {
+  const eggInSlot1 = document.querySelector(".slot-one");
+  const eggInSlot2 = document.querySelector(".slot-two");
+  const eggInSlot3 = document.querySelector(".slot-three");
 
   if (eggInSlot1) {
-    eggInSlot1.classList.add("moveOneToTwo");
-    eggInSlot1.addEventListener("animationend", moveOneToTwo, { once: true });
+    eggInSlot1.style.animation = `1s ease one2two forwards`;
+    eggInSlot1.addEventListener(
+      "animationend",
+      () => {
+        return move(eggInSlot1, "one", "two");
+      },
+      {
+        once: true,
+      }
+    );
   } else {
     console.log("no egg in slot1");
   }
 
   if (eggInSlot2) {
-    eggInSlot2.classList.add("moveTwoToThree");
-    eggInSlot2.addEventListener("animationend", moveTwoToThree, { once: true });
+    eggInSlot2.style.animation = `1s ease two2three forwards`;
+    eggInSlot2.addEventListener(
+      "animationend",
+      () => {
+        return move(eggInSlot2, "two", "three");
+      },
+      { once: true }
+    );
   } else {
     console.log("no egg in slot2");
   }
 
   if (eggInSlot3) {
-    eggInSlot3.classList.add("moveThreeToOne");
-    eggInSlot3.addEventListener("animationend", moveThreeToOne, { once: true });
+    eggInSlot3.style.animation = `1s ease three2one forwards`;
+    eggInSlot3.addEventListener(
+      "animationend",
+      () => {
+        return move(eggInSlot3, "three", "one");
+      },
+      { once: true }
+    );
   } else {
     console.log("no egg in slot3");
   }
-
-  function moveOneToTwo() {
-    console.log("animation done");
-    eggInSlot1.classList.remove("slot1", "moveOneToTwo");
-    eggInSlot1.classList.add("slot2");
-  }
-  function moveTwoToThree() {
-    console.log("animation done");
-    eggInSlot2.classList.remove("slot2", "moveTwoToThree");
-    eggInSlot2.classList.add("slot3");
-  }
-  function moveThreeToOne() {
-    console.log("animation done");
-    eggInSlot3.classList.remove("slot3", "moveThreeToOne");
-    eggInSlot3.classList.add("slot1");
-  }
 }
 
-btnLeft.addEventListener("click", rotateEggs);
+btnRight.addEventListener("click", rotateEggsToRight);
