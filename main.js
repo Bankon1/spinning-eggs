915b4f0c8472818811fe41c9695458a5c2da9d0e
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-02-15 10:40:32 +0100

    Added Superspin

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const btnLeft = document.querySelector(".btn-left");
 const btnRight = document.querySelector(".btn-right");
+const btnRandomSpin = document.querySelector(".random-spin-btn");
 
 let position = 1;
 const time = "1.5s";
@@ -71,14 +72,33 @@ function rotateEggsToRight() {
     );
     plate.addEventListener("animationend", () => {
       position = 2;
-      console.log(position);
     });
   }
 }
 
 function randomSpin() {
+  const time = "2s";
+  const plate = document.querySelector(".plate");
+  const electrons = document.querySelectorAll(".electron");
+  plate.style.animation = `plateFullTurn ${time} linear forwards`;
+  electrons.forEach(
+    (electron) =>
+      (electron.style.animation = `itemFullTurn ${time} linear forwards`)
+  );
+  plate.addEventListener(
+    "animationend",
+    () => {
+      plate.style.animation = "";
+      electrons.forEach((electron) => (electron.style.animation = ""));
+    },
+    {
+      once: true,
+    }
+  );
+
   
 }
 
 btnLeft.addEventListener("click", rotateEggsToLeft);
 btnRight.addEventListener("click", rotateEggsToRight);
+btnRandomSpin.addEventListener("click", randomSpin);
